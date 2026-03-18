2bc695381763f8840b9b6a1d298904145a2afe81
Rohit-Deshpande7 <rvdeshpande301198@gmail.com>
2026-03-18 23:33:25 +0530

    0th Commit

--- src/main/java/javaCodes/SimpleJavaCodes/First.java
+++ src/main/java/javaCodes/SimpleJavaCodes/First.java
@@ -1,2 +1,8 @@
-package javaCodes.SimpleJavaCodes;public class First {
+package javaCodes.SimpleJavaCodes;
+
+public class First {
+    public static void main(String []args)
+    {
+        System.out.println("Checking the git repo");
+    }
 }
