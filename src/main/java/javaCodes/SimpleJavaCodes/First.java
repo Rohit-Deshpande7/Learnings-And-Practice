1ca8c0b0e9a74c4ab95bb68878aa9df74a50beef
Rohit <rvdeshpande301198@gmail.com>
2026-03-19 00:01:15 +0530

    main branch 0th commmit

--- src/main/java/javaCodes/SimpleJavaCodes/First.java
+++ src/main/java/javaCodes/SimpleJavaCodes/First.java
@@ -3,6 +3,6 @@ package javaCodes.SimpleJavaCodes;
 public class First {
     public static void main(String []args)
     {
-        System.out.println("Checking the git repo");
+        System.out.println("MAIN BRANCH");
     }
 }
